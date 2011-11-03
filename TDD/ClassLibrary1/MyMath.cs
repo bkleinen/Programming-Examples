298d77c6e75f2677e86cb2e87ad770463aef092a
Bela Kleinen <kleinen@infrastructure.de>
2011-11-03 11:17:05 +0100

    initial commit -tdd

---
+++ TDD/ClassLibrary1/MyMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class MyMath
+    {
+        public int GreatestCommonDivisor(int a, int b) {
+            if (a < b) return GreatestCommonDivisor(b, a);
+            if ((a % b) == 0) return b;
+            return GreatestCommonDivisor(a - b, b);
+        }
+    }
+}
---
+++ TDD/ClassLibrary1/MyMathTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    class MyMathTest
+    {
+        [Test]
+        public void GreatestCommonDivisor() {
+            MyMath math = new MyMath();
+            Assert.That(math.GreatestCommonDivisor(1, 1), Is.EqualTo(1));
+            Assert.That(math.GreatestCommonDivisor(6, 3), Is.EqualTo(3));
+            Assert.That(math.GreatestCommonDivisor(14, 21), Is.EqualTo(7));
+            Assert.That(math.GreatestCommonDivisor(144, 48), Is.EqualTo(48));
+            Assert.That(math.GreatestCommonDivisor(144, 60), Is.EqualTo(12));
+            Assert.That(math.GreatestCommonDivisor(21, 15), Is.EqualTo(3));
+            Assert.That(math.GreatestCommonDivisor(57, 7), Is.EqualTo(1));
+        }
+
+    }
+}
